30c64b236d8f160acf764dc788d0e92f53372bd6
Cindy Nainggolan <72927218+rosieposiecngl@users.noreply.github.com>
2022-12-20 11:49:05 +0700

    Update detail_dokter.dart

--- lib/components/detail_dokter.dart
+++ lib/components/detail_dokter.dart
@@ -10,7 +10,7 @@ class DetailDokter extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Text(
-        _description,
+        'Dokter Jennie adalah ahli bedah jantung teratas di Rumah Sakit Heart. Dia telah melakukan lebih dari 100 operasi yang sukses dalam 3 tahun terakhir.\nDia telah meraih beberapa penghargaan atas kontribusinya yang luar biasa di bidangnya sendiri. Dia tersedia untuk konsultasi pribadi untuk jadwal tertentu.',
         style: TextStyle(
           color: kTitleTextColor.withOpacity(0.7),
           height: 1.6,
